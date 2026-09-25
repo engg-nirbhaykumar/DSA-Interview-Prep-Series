95964c5ef80e07bd3bbd9d8c3ee655e05168219c
Nirbhay Kumar <67179429+engg-nirbhaykumar@users.noreply.github.com>
2026-09-25 09:00:10 +0530

    Time: 0 ms (100%), Space: 46.4 MB (61.71%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,21 +1,15 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
+        int size = 2 * n;
+        int left = 0;
+        int right = n;
+        int[] ans = new int[size];
 
-        // Create result array of size 2 * n
-        int[] ans = new int[2 * n];
-
-        // i -> index for result array
-        // left -> points to first half [x1, x2, ..., xn]
-        // right -> points to second half [y1, y2, ..., yn]
-        int i = 0, left = 0, right = n;
-
-        // Pick one element from first half and then one from second half
-        while (left < n) {
-            ans[i++] = nums[left++]; // Add xi
-            ans[i++] = nums[right++]; // Add yi
+        for (int i = 0; i < size; i++) {
+            ans[i] = nums[left++];
+            i++;
+            ans[i] = nums[right++];
         }
-
-        // Return shuffled array
         return ans;
     }
 }
